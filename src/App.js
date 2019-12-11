24074dd36b8e3df5bdb8113301d0a950ef016749
Thipparthi <MT057426@cerner.net>
2019-12-11 11:49:15 -0600

    push

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import $ from 'jquery';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
@@ -16,17 +17,23 @@ class App extends Component{
     };
   }
 
-  componentDidMount() {
-    fetch("/resumeData.json")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({resumeData: result});
-        },
-        (error) => {
-          this.setState(error);
-        }
-      )
+  getResumeData(){
+    $.ajax({
+      url:'/resumeData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getResumeData();
   }
 
   render(){
